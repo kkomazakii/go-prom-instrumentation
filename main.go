c23bc3594144ba93e18937777eec07ae97766cb6
kkomazakii <komazaki@giftee.co>
2019-07-30 16:54:33 +0900

    grpc を追加しました
    port を覚えるのがだるいので引数で指定するようにしました

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net"
 	"net/http"
+	"os"
 	"time"
+
+	pb "github.com/kkomazakii/go-prom-instrumentation/pb"
+	"google.golang.org/grpc"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+type server struct{}
 
 func recordMetrics() {
 	// go chan って即時実行できるんけ?
@@ -26,9 +35,32 @@ var (
 	})
 )
 
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.Name)
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func (s *server) SayHelloAgain(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello again " + in.Name}, nil
+}
+
 func main() {
 	recordMetrics()
 
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
+	// ListenAndServe で block してしまうので別の thread でやる
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":2112", nil)
+	}()
+
+	port := os.Args[1]
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
